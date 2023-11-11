3d787253a23fb107cbf62ee2dcbf89fcc830b7f4
bakshay96 <akshaymali.edu@gmail.com>
2023-11-11 19:33:36 +0530

    backend with AB

--- currency-converter/src/hooks/useCurrencyInfo.js
+++ currency-converter/src/hooks/useCurrencyInfo.js
@@ -7,10 +7,12 @@ function useCurrencyInfo(currency){
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
-        console.log(data);
+        console.log("curreny data",data);
     }, [currency])
     console.log(data);
     return data
 }
+
+
 
 export default useCurrencyInfo;
